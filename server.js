e1e71c3c460dfe963c290b423201c6c60f3d6a5e
DevShashi1993 <shashikantsharma1993@gmail.com>
2020-08-31 03:04:17 +0530

    Project Initialization
    -Installed and configured express, nodemon, socket.io
    -Added basic layout for chat-app

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { log } = require('console');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+});
+
+app.use(express.static(__dirname + '/public/'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+
+// configure socket.io
+io.on('connection', (socket)=> {
+    io.log('Connected...');
+})
+
+
